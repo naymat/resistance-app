fd319b4c75e2d090af94aac1087f32eecdfe38a4
Naymat <naymat30@gmail.com>
2018-07-24 01:17:34 -0400

    Added game class to game.js

--- game.js
+++ game.js
@@ -1,3 +1,27 @@
+class Game{
+    constructor(){
+        this.playerList = [];
+        this.roundNumber = 0;
+    }
+
+    /**
+     * 
+     * @param {Player} player - player to be registered to the game
+     */
+    addPlayer(player){
+        this.playerList.push(player);
+    }
+    /**
+     * Returns Array with the current Player objects
+     */
+    getPlayers(){
+        return this.playerList;
+    }
+    /**
+     * 
+     */
+}
+
 class Player{
     constructor(id, name){
         this.id = id;
@@ -9,13 +33,14 @@ class Player{
     getName(){
         return this.name;
     }
-    createSpy(){
+    assignAsSpy(){
         this.type = 'Spy';
     }
-    createResistanceMember(){
+    assignAsResistanceMember(){
         this.type = 'Resistance';
     }
 }
+const resist  = new Game();
 
 var p1 = new Player(1,'Naymat');
 console.log(p1.getName());
